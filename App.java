3851036e1eaab9a25a7639321357950cfd815f52
Rakesh kumar mohanty <111591636+javaproject123@users.noreply.github.com>
2022-08-20 11:56:29 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,12 @@
+package com.java;
+
+public class App {
+
+	public static void main(String[] args) {
+		double radious=6.7;
+		double area=3.14*radious*radious;
+		System.out.println(area);
+		
+	}
+
+}
