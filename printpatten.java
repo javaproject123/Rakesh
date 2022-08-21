06ef9913ad425a9a040e7a4e54bd30107935c156
Rakesh kumar mohanty <111591636+javaproject123@users.noreply.github.com>
2022-08-21 22:20:38 +0530

    Add files via upload

---
+++ printpatten.java
@@ -0,0 +1,14 @@
+package com.java;
+
+public class printpatten {
+
+	public static void main(String[] args) {
+		System.out.println("*");
+		System.out.println("**");
+		System.out.println("***");
+		System.out.println("****");
+		System.out.println("*****");
+		
+	}
+
+}
