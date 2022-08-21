21c66e55a3c1421a9798cd1f435f7bff4bcd8d3a
Rakesh kumar mohanty <111591636+javaproject123@users.noreply.github.com>
2022-08-21 21:37:18 +0530

    Add files via upload

---
+++ condition.java
@@ -0,0 +1,12 @@
+package com.java;
+
+public class condition {
+
+	public static void main(String[] args) {
+		int num=20;
+		String result=num>=19?"positive number":"negetive number";
+		System.out.println(result);
+		
+	}
+
+}
