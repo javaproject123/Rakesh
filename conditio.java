f81074657f6b78422f01bd1bfbc262f90e3d49f7
Rakesh kumar mohanty <111591636+javaproject123@users.noreply.github.com>
2022-08-23 09:48:49 +0530

    Add files via upload

---
+++ conditio.java
@@ -0,0 +1,17 @@
+package com.java;
+import java.util.*;
+public class conditio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Scanner sc=new Scanner(System.in);
+       int age=sc.nextInt();
+       if(age>18) {
+    	   System.out.println("Adult"); 
+       }
+       else {
+    	   System.out.println("not Adult");
+       }
+	}
+
+}
