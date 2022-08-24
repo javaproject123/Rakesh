d50a5f03b7e4d765d8ef32f3c18f292acde8595c
Rakesh kumar mohanty <111591636+javaproject123@users.noreply.github.com>
2022-08-24 09:58:02 +0530

    Add files via upload

---
+++ loop.java
@@ -0,0 +1,15 @@
+package com.java;
+import java.util.*;
+public class loop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Scanner sc=new Scanner(System.in);
+       int n=sc.nextInt();
+       for(int i=1;i<11;i++) {
+    	   System.out.println(i*n);
+    	   //n=2 table
+       }
+	}
+
+}
