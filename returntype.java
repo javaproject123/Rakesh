13c31d9f9f0b8bf1a47e0914e0c4f0bef7365549
Rakesh kumar mohanty <111591636+javaproject123@users.noreply.github.com>
2022-08-25 22:26:25 +0530

    Add files via upload

---
+++ returntype.java
@@ -0,0 +1,18 @@
+package com.java;
+import java.util.*;
+public class returntype {
+    public static int calculation(int a,int b) {
+    	int sum=a+b;
+    	return sum;
+    	
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      Scanner sc=new Scanner(System.in);
+      int a=sc.nextInt();
+      int b=sc.nextInt();
+      int sum = calculate sum(a,b);
+      System.out.println("sum of 2number is:"+sum);
+	}
+
+}
