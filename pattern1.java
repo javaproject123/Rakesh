e446dc2839a178224f7f25c5cb4dd369d93ee794
Rakesh kumar mohanty <111591636+javaproject123@users.noreply.github.com>
2022-08-25 22:43:39 +0530

    Add files via upload

---
+++ pattern1.java
@@ -0,0 +1,15 @@
+package com.java;
+
+public class pattern1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int count=1;
+       for(int i=1;i<=5;i++) {
+    	   for(int j=1;j<=i;j++) {
+    		   System.out.print(count+" ");
+    	   }
+       }     System.out.println();
+	}
+
+}
