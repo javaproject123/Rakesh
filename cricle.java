5102f858425876986d5128d6f1ba53724713c59c
Rakesh kumar mohanty <111591636+javaproject123@users.noreply.github.com>
2022-08-26 20:02:09 +0530

    Add files via upload

---
+++ cricle.java
@@ -0,0 +1,15 @@
+package com.java;
+
+public class cricle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       double r;
+       double pi=3.142, area;
+       r=10;
+       area = pi*r*r;
+       System.out.println("Area of circle is:"+area);
+       
+	}
+
+}
