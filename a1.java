a85288b5ad588713e44c4533f5fc9d50814be4f8
Rakesh kumar mohanty <111591636+javaproject123@users.noreply.github.com>
2022-08-26 19:09:29 +0530

    Add files via upload

---
+++ a1.java
@@ -0,0 +1,12 @@
+package com.java;
+
+public class a1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int a=20,b=30,c;
+       c=a+b;
+       System.out.println("Sum is="+c);
+	}
+
+}
