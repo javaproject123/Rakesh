474a313d9c43290fefd2d43408dc8ba67db6b26f
Rakesh kumar mohanty <111591636+javaproject123@users.noreply.github.com>
2022-08-26 19:55:26 +0530

    Add files via upload

---
+++ celsius.java
@@ -0,0 +1,13 @@
+package com.java;
+
+public class celsius {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         float f,c;
+         f=120;
+         c=((f-32)*5)/9;
+         System.out.println("Temp in celsius:"+c);
+	}
+
+}
