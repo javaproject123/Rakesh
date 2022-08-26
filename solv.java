3a4a398d335b53aec49e89a632c706563e65f4e3
Rakesh kumar mohanty <111591636+javaproject123@users.noreply.github.com>
2022-08-26 19:17:42 +0530

    Add files via upload

---
+++ solv.java
@@ -0,0 +1,16 @@
+package com.java;
+
+public class solv {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int n1=30,n2=50;
+        if(n1>n2) {
+        	System.out.println(n1+"is larger");
+        	
+        }
+        else {System.out.println(n2+" is larger");
+	}
+
+ }
+}
