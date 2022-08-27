26283ebba13d22947cb8d280bbf55e7033048d87
Rakesh kumar mohanty <111591636+javaproject123@users.noreply.github.com>
2022-08-27 17:52:01 +0530

    Add files via upload

---
+++ cube.java
@@ -0,0 +1,16 @@
+package com.java;
+import java.util.*;
+public class cube {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter a number - ");
+    int n=sc.nextInt();
+    for(int i=1;i<=n;i++) {
+    	  System.out.println("cube of "+i+" is "+i*i*i);
+    	  
+    }
+	}
+
+}
